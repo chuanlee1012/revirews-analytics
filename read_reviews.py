00b4f321d66d38f01318d3aaa56048b2ef6bb396
Chuan Lee <scott900124@gmail.com>
2019-12-29 12:24:27 +0800

    calc avg length of reviews

--- read_reviews.py
+++ read_reviews.py
@@ -1,9 +1,14 @@
 data = []
 count = 0
-with open ('reviews.txt', 'r') as f:
+with open ('reviews.txt', 'r') as f: # as 當作
 	for line in f:
 		data.append(line)
 		count += 1 # count = count + 1
-		if count % 1000 == 0: # % 是用來求餘數
+		if count % 100000 == 0: # % 是用來求餘數
 			print(len(data))
-print(len(data))
+print('檔案讀取完畢，總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d) #sum_len = sum_len + len(d)
+print('留言的平均長度是', sum_len/len(data))	
